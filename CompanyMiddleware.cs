0e6fd953d7e32442fb61e6da52bef2f67f4bd078
trhggS <159827208+trhggS@users.noreply.github.com>
2024-02-12 21:52:53 +0200

    lr1

---
+++ CompanyMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json;
+
+public class CompanyMiddleware
+{
+    private readonly RequestDelegate _next;
+    private Company _company;
+
+    public CompanyMiddleware(RequestDelegate next)
+    {
+        _next = next;
+
+        _company = new Company
+        {
+            Id = 1,
+            Name = "Example Company",
+            Description = "This is an example company."
+        };
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Path == "/company")
+        {
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(_company));
+        }
+        else if (context.Request.Path == "/random")
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(0, 101); // Випадкове число від 0 до 100
+
+            await context.Response.WriteAsync($"Random Number: {randomNumber}");
+        }
+        else
+        {
+            await _next.Invoke(context);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,27 @@
+﻿public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseMiddleware<CompanyMiddleware>();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        });
+    }
+}
